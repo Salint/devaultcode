a43dd397b3deb249decf8fefd133aeb65ddfb37a
Mohamed Abduljawad <salintofficial@hotmail.com>
2023-06-14 06:21:58 +0300

    Add router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
 
 import "./style.css";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<h1>Hello World</h1>);
+const App = () => {
+
+	const router = createBrowserRouter(
+		createRoutesFromElements(
+			<>
+				<Route exact path="/" element={<h1>Test</h1>} />
+			</>
+		)
+	);
+
+	return <RouterProvider router={router} />
+};
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
