42661347db73ce87f39485f4053f147419c4d75b
Mohamed Abduljawad <salintofficial@hotmail.com>
2023-06-15 14:41:27 +0300

    Add a modal molecule

---
+++ src/components/molecules/Modal.js
@@ -0,0 +1,86 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled("div")`
+	position: fixed;
+	top: 0;
+	left: 0;
+	width: 100vw;
+	height: 100vh;
+`;
+
+const Background = styled("div")`
+	position: absolute;
+	z-index: 1;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	background: black;
+	opacity: 0.3;
+`;
+
+const Window = styled("section")`
+	position: absolute;
+	z-index: 2;
+	top: 50%;
+	left: 50%;
+	width: ${props => props.width + "px"};
+	height ${props => props.height + "px"};
+	transform: translate(-50%, -50%);
+	background: var(--background-primary);
+	border-radius: 10px;
+	box-shadow: 7px 7px 20px #999;
+	border: 1px solid var(--border);
+	overflow: hidden;
+`;
+
+const TitleBar = styled("div") `
+	width: 100%;
+	background: var(--background-secondary);
+	border-bottom: 1px solid var(--border);
+	display: flex;
+	align-items: center;
+	padding: 10px;
+`;
+const FlexItem = styled("div")`
+	flex: 1;
+	display: flex;
+	align-items: center;
+	justify-content: ${props => props.align};
+`;
+const Title = styled("h1")`
+	font-size: 16px;
+	margin-left: 10px;
+`;
+const Button = styled("button")`
+	border: none;
+	background: var(--background-primary);
+	color: black;
+	border: 1px solid var(--border);
+	font-size: 13px;
+	cursor: pointer;
+	padding: 7px 10px;
+	border-radius: 50%;
+`;
+
+const Modal = ({ closeModal, width, height, title }) => {
+
+	return (
+		<Container>
+			<Background onClick={e => closeModal()}/>
+			<Window width={width} height={height}>
+				<TitleBar>
+					<FlexItem align="flex-start">
+						<Title>{title}</Title>
+					</FlexItem>
+					<FlexItem align="flex-end">
+						<Button onClick={e => closeModal()}>X</Button>
+					</FlexItem>
+				</TitleBar>
+			</Window>
+		</Container>
+	);
+}
+
+export default Modal;
--- src/components/organisms/Header.js
+++ src/components/organisms/Header.js
@@ -1,11 +1,12 @@
 import { faPlus, faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 import LogoSrc from "../../assets/logo.png";
 import AuthService from "../../services/AuthService";
 import Button from "../atoms/Button";
+import Modal from "../molecules/Modal";
 
 const Container = styled("header")`
 	width: 600px;
@@ -41,8 +42,11 @@ const Header = () => {
 
 	const authService = new AuthService();
 
+	const [ modalOpened, setModalOpened ] = useState(false);
+
 	return (
 		<Container>
+			{modalOpened && <Modal title="Create a new list" width="500" height="250" closeModal={() => setModalOpened(false)}/>}
 			<FlexItem align="flex-start">
 				<Action onClick={async e => await authService.logOut()}><FontAwesomeIcon icon={faRightFromBracket} /></Action>
 			</FlexItem>
@@ -50,7 +54,7 @@ const Header = () => {
 				<Link to="/"><Img src={LogoSrc} /></Link>
 			</FlexItem>
 			<FlexItem align="flex-end">
-				<Action onClick={async e => await authService.logOut()}><FontAwesomeIcon icon={faPlus} /></Action>
+				<Action onClick={e => setModalOpened(!modalOpened)}><FontAwesomeIcon icon={faPlus} /></Action>
 			</FlexItem>
 		</Container>
 	)
